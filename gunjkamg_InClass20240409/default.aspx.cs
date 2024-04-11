cbe47229300944d0084f57e0a550556c7c43e13b
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-04-11 11:45:19 -0400

    Add project files.

---
+++ gunjkamg_InClass20240409/default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace gunjkamg_InClass20240409
+{
+
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Check if the page is not a postback, which means it's the initial load
+            if (!IsPostBack)
+            {
+                // Since it's the initial load, add items to the ListBox
+                lbFruits.Items.Add("Strawberry");
+                // Add more fruits as needed
+            }
+        }
+
+        // Event that will run when the user in their browser selects a fruit from their list box
+        protected void lbFruits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblChosenFruit.Text = lbFruits.SelectedValue;
+
+            // Do something with the selected fruit, for example:
+            // Response.Write(selectedFruit);
+        }
+    }
+}
